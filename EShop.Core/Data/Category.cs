2f69fbbcc64a4d572f0dcebfb23983029060bb4e
Mateusz <matbagilcz@gmail.com>
2018-04-06 10:10:00 +0200

    added categoryname to category

--- EShop.Core/Data/Category.cs
+++ EShop.Core/Data/Category.cs
@@ -5,6 +5,7 @@ namespace EShop.Core.Data
     public class Category : BaseEntity<int>
     {              
         public int? ParentId {get;set;}
+        public string CategoryName{get;set;}  
         public virtual Category Parent{get;set;}
       }
 }
