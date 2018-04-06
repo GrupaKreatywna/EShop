ecef375860a8f5a74d40bcfaadd4a3435ed19e7e
Paweł Halicki <pawelhal12@gmail.com>
2018-04-06 09:44:58 +0200

    Updated Product

--- EShop.Core/Entities/Product.cs
+++ EShop.Core/Entities/Product.cs
@@ -6,11 +6,11 @@ namespace EShop.Core.Entities
 {
     public class Product : BaseEntity<int>
     {
-        string Name{get;set;}
-        string Picture{get;set;}
-        string Description{get;set;}
-        string Tags{get;set;}
-        int Count{get;set;}
+        public string Name{get;set;}
+        public string Picture{get;set;}
+        public string Description{get;set;}
+        public string Tags{get;set;}
+        public int Count{get;set;}
         public ICollection<Price> Prices{get;set;}
 
         //public virtual Category Category{get;set;}                   
--- EShop.Data/Migrations/20180406074223_PP_migv2.Designer.cs
+++ EShop.Data/Migrations/20180406074223_PP_migv2.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace EShop.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20180406074223_PP_migv2")]
+    partial class PP_migv2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
