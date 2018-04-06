dcf85e8d70ec2e1a6b429f30536f5da2f21faae0
Michal B <maximgt540@gmail.com>
2018-04-06 10:51:03 +0200

    Add DiscountCoupon entity

---
+++ EShop.Core/Entities/DiscountCoupon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Eshop.Core.Entities;
+
+namespace EShop.Core.Entities
+{
+    public class DiscountCoupon : BaseEntity<int>
+    {
+        public string Name { get; set; }
+
+        public int CouponCode { get; set; }
+
+        public DateTime ValidationStart { get; set; }
+
+        public DateTime ValidationEnd { get; set; }
+
+        //public ICollection<Order> Orders { get; set; }
+    }
+}
