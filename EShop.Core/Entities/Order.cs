53e4c61d793d006ddafc898b50cbbf1b36dcb5d9
Unknown <vaporbae@gmail.com>
2018-04-06 09:30:50 +0200

    Added Order

---
+++ EShop.Core/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Eshop.Core.Entities;
+
+namespace EShop.Core.Entities
+{
+    public class Order : BaseEntity<int>
+    {
+        
+        public DateTime OrderDate { get; set; }
+        public string Adress { get; set; }
+        public string ContractingAuthority { get; set; }
+        public string City { get; set; }
+        public  string PostalCode { get; set; }
+        //public int DiscountCouponId {get;set;}
+        //public int DiscountCoupon DiscountCoupon {get;set;}
+    }
+}
