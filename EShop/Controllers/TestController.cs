9fbf7bb52d84cb8786185c65670538f70c1809cf
Andrzej Gołaszewski <golaszewski.a@gmail.com>
2018-04-06 12:27:33 +0200

    sample

--- EShop/Controllers/TestController.cs
+++ EShop/Controllers/TestController.cs
@@ -1,13 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Test.API.Controllers.Configuration;
+//using Test.Core.CQRS;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
 
-namespace EShop.Controllers
-{
-    public class TestController : Controller
-    {
-        
-    }
-}
+//namespace Test.API.Controllers
+//{
+//    [Route("api/[controller]")]
+//    public class ConfigurationController : Controller
+//    {
+//        private IQueryDispatcher _queryDispatcher;
+//        private ICommandDispatcher _commandDispatcher;
+
+//        public ConfigurationController(IQueryDispatcher queryDispatcher, ICommandDispatcher commandDispatcher)
+//        {
+//            _queryDispatcher = queryDispatcher;
+//            _commandDispatcher = commandDispatcher;
+//        }
+
+//        [HttpGet("[action]")]
+//        public async Task<List<GetVersions.Result>> Versions()
+//        {
+//            return await _queryDispatcher.Dispatch<GetVersions.Query, List<GetVersions.Result>>(new GetVersions.Query());
+//        }
+
+//    }
+//}
