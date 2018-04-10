e6c25b1288ddb3fe7a64f027c65ab782c65c2e38
Artur T <atagisov@gmail.com>
2018-04-10 11:08:56 +0200

    add Homepage container for BasicUI components

--- EShop.Frontend/ClientApp/src/components/Home.js
+++ EShop.Frontend/ClientApp/src/components/Home.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {UnnumberedList, SearchAutocomplete, Products} from './BasicUI.js';
 
-export class Home extends Component {
-  displayName = Home.name
+let placeholderCategories = [
+  { id: '1', name: "category1placeholder" },
+  { id: '2', name: "foo bar category !@# $ @ !" },
+  { id: '3', name: "category3" }
+];
 
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we've also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
-}
+let placeholderItems = [
+  { id: '1', img:'https://placehold.it/32x32', productname: "przykladowy produkt", },
+  { id: '2', img:'https://placehold.it/32x32', productname: "Exiting Vim: Theory and Practice" },
+  { id: '3', img:'https://placehold.it/32x32', productname: "Podstawy Elektroniki i Elektrotechniki dla Studentów Informatyki" }
+];
+
+export const Homepage = () => (
+  <div>
+      <UnnumberedList data={placeholderCategories} display='name' primaryKey='id'/>
+      <SearchAutocomplete searchThrough={placeholderItems} display='productname' primaryKey='id' />
+      <Products data={placeholderItems} nameProp='productname' imgProp='img' primaryKey='id'/>
+  </div>
+)
