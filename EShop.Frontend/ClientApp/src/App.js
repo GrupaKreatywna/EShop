832f4e059509b277cfb04369943eef1e8765ab1f
Artur T <atagisov@gmail.com>
2018-04-12 20:47:55 +0200

    Remove old template layout

--- EShop.Frontend/ClientApp/src/App.js
+++ EShop.Frontend/ClientApp/src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
 import { Homepage } from './components/Home';
 import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 
 export default class App extends Component {
   displayName = App.name
 
   render() {
     return (
-      <Layout>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
-      </Layout>
+        <div>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/fetchdata' component={FetchData} />
+        </div>
     );
   }
 }
