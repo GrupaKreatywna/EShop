0045b1c61753653a1c8eb6bf4c0b1e436823c68d
Paweł Halicki <pawelhal12@gmail.com>
2018-04-13 15:18:49 +0200

    Add PriceControler with Queries and Commands

---
+++ EShop/Controllers/Price/CreatePrice.cs
@@ -0,0 +1,64 @@
+﻿using Eshop.Core.CQRS;
+using Eshop.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Controllers.Price
+{
+    public class CreatePrice
+    {
+        public class Command : ICommand
+        {
+            public Data _data;
+        }
+
+        public class Handler : ICommandHandler<Command>
+        {
+            private readonly IUnitOfWork _uow;
+
+            public Handler(IUnitOfWork uow)
+            {
+                _uow = uow;
+            }
+
+            public async Task Execute(Command command)
+            {
+                _uow.PriceRepository.Insert(command._data.ToPriceEntity());
+                await _uow.SaveChangesAsync();
+            }
+        }
+
+
+        public class Data
+        {
+            public decimal Value { get; set; } 
+            public DateTime StartDate { get; set; }
+            public DateTime? EndDate { get; set; }
+
+            public Data(decimal value, DateTime startDate, DateTime? endDate)
+            {
+                this.Value = value;
+                this.StartDate = startDate;
+                this.EndDate = endDate;
+            }
+
+            public Core.Entities.Price ToPriceEntity()
+            {
+                var price = new Core.Entities.Price
+                {
+                    Value = this.Value,
+                    StartDate = this.StartDate,
+                    EndDate = this.EndDate
+                };
+                return price;
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ EShop/Controllers/PriceController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eshop.Core.CQRS;
+using EShop.Controllers.Price;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Controllers
+{
+    [Route("api/[controller]")]
+    public class PriceController : Controller
+    {
+        private readonly IQueryDispatcher _queryDispatcher;
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public PriceController(IQueryDispatcher queryDispatcher, ICommandDispatcher commandDispatcher)
+        {
+            _queryDispatcher = queryDispatcher;
+            _commandDispatcher = commandDispatcher;
+        }
+
+        [HttpGet("/api/Prices")]
+        public async Task<List<GetAllPrices.Result>> GetAll()
+        {
+            return await _queryDispatcher.Dispatch<GetAllPrices.Query, List<GetAllPrices.Result>>(new GetAllPrices.Query());
+        }
+
+        [HttpGet("/api/Price/{id}")]
+        public async Task<GetPrice.Result> Get(int id)
+        {
+            return await _queryDispatcher.Dispatch<GetPrice.Query, GetPrice.Result>(new GetPrice.Query(id));
+        }
+
+        [HttpPost("")]
+        public async Task Create(decimal value, DateTime startDate, DateTime? endDate )
+        {
+             await _commandDispatcher.Dispatch<CreatePrice.Command>(new CreatePrice.Command()
+            {
+                _data = new CreatePrice.Data(value, startDate, endDate)
+
+            });
+
+        }
+    }
+}
