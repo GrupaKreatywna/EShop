a3cb61f864788874dd6b2315fe2650e13d48472b
Artur T <atagisov@gmail.com>
2018-04-14 14:10:59 +0200

    Fix <Products/> being bigger than their wrapper
    They didn't inherit the height from their parent

--- EShop.Frontend/ClientApp/src/components/Home/index.jsx
+++ EShop.Frontend/ClientApp/src/components/Home/index.jsx
@@ -18,13 +18,16 @@ export const Home = () => (
     <div className={style.gridCategories}>
       <UnnumberedList/>
     </div>
+    
     <div className={style.gridSearchAutocomplete}>  
       <SearchAutocomplete/>
     </div>
+
     <div className={style.gridProductsMostPopular}>  
       <p><b>Najczęściej kupowane</b></p>
       <Products/>
     </div>
+    
     <div className={style.gridProductsRecentlyAdded}>
       <p><b>Ostatnio dodane</b></p>
       <Products/>
