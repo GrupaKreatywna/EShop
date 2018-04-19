8c47e4531e7858f15484986de17d300e00c2a96d
Paweł Halicki <pawelhal12@gmail.com>
2018-04-19 12:11:18 +0200

    Add Redis

---
+++ EShop/Controllers/BucketController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Controllers
+{
+    [Route("api/[controller]")]
+    public class BucketController : Controller
+    {
+       
+
+
+    }
+}
