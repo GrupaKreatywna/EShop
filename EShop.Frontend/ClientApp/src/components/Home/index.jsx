1b593d6d4dfc0687d16076cd7595737820e6a983
Artur T <atagisov@gmail.com>
2018-04-26 12:45:36 +0200

    Remove old, pre-api JSON product placeholders

--- EShop.Frontend/ClientApp/src/components/Home/index.jsx
+++ EShop.Frontend/ClientApp/src/components/Home/index.jsx
@@ -7,19 +7,6 @@ import UnnumberedList from '../UnnumberedList/'; //TODO: webpack aliases (to reo
 import SearchAutocomplete from '../SearchAutocomplete';
 import Products from '../Products/';
 
-
-let placeholderCategories = [
-  { id: '1', name: "category1placeholder" },
-  { id: '2', name: "foo bar category !@# $ @ !" },
-  { id: '3', name: "category3" }
-];
-
-let placeholderItems = [
-  { id: '1', img:'https://placehold.it/32x32', productname: "przykladowy produkt", },
-  { id: '2', img:'https://placehold.it/32x32', productname: "Exiting Vim: Theory and Practice" },
-  { id: '3', img:'https://placehold.it/32x32', productname: "Podstawy Elektroniki i Elektrotechniki dla Studentów Informatyki" }
-];
-
 export const Home = () => (
   <div className={style.Layout}>
 
