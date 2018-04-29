9d2654884c7e3181af95b59bd50a6d9aea38f881
Artur T <atagisov@gmail.com>
2018-04-29 14:34:59 +0200

    Add ... please dont look at the changes

--- EShop.Frontend/ClientApp/src/components/ProductDetails/index.jsx
+++ EShop.Frontend/ClientApp/src/components/ProductDetails/index.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 import style from './style.css'
 import * as env from '../../env.js'
 
-//TODO add item counter
 //TODO handle what happens when the id is invalid (doesnt exist in db/is negative etc)
 //TODO handle invalid fetches (this should be the same as the above TODO)
 
@@ -20,6 +19,23 @@ export class ProductDetails extends Component {
 
         this.idRouteParam = this.props.match.params.id; // this is the ID of the product this component views (from URL - localhost/product/[:id])        
         this.addProductToCart = this.addProductToCart.bind(this);
+
+        const Counter = props => {
+            const changeCounterBy = delta => { 
+                let currentCopies = this.state.numberOfCopiesToBuy;
+                if(currentCopies+delta <= 0) return;
+                this.setState({numberOfCopiesToBuy: currentCopies+delta}) 
+            }
+    
+            const one = 1;
+            return( //TODO Add classNames to ProductDetails counter divs
+                <div>
+                    <button onClick={() => changeCounterBy(one)}>+{one}</button>
+                    <button onClick={() => changeCounterBy(-one)}>{-one}</button>
+                    <div>{this.state.numberOfCopiesToBuy}</div>
+                </div>
+            )
+        }
     }
 
     async componentDidMount() {
@@ -28,7 +44,7 @@ export class ProductDetails extends Component {
         let getPrice = async () => await(await fetch(env.host+env.apiSinglePrice+this.state.product[env.product.currentPriceId])).json();
         
         this.setState({product: await getProduct()}); //TODO figure out if this can be done in one setState
-        this.setState({price: (await getPrice())[env.price.value]});
+        this.setState({price: (await getPrice())[env.price.value]}); // ! getPrice() is dependent on getProduct()
     }
 
 
@@ -65,26 +81,6 @@ export class ProductDetails extends Component {
     }
 
     render() {
-        
-        const Counter = props => {
-            const changeCounter = delta => { 
-                let currentCopies = this.state.numberOfCopiesToBuy;
-                if(currentCopies+delta <= 0) 
-                    return;
-                this.setState({numberOfCopiesToBuy: this.state.numberOfCopiesToBuy+delta}) 
-            }
-    
-            const p = 1;
-            const m = -1;
-            return( //TODO Add classNames to ProductDetails counter divs
-                <div>
-                    <button onClick={() => changeCounter(p)}>+{p}</button>
-                    <button onClick={() => changeCounter(m)}>{m}</button>
-                    <div>{this.state.numberOfCopiesToBuy}</div>
-                </div>
-            )
-        }
-        
         return (
             <div id="wrapper">
                 <img id="image" src={this.state.product[env.product.img]} alt={this.state.product[env.product.name]} />
