bee0b27b2af4f29dae64c262c48ce7bc43c0fdbc
Artur T <atagisov@gmail.com>
2018-04-30 18:31:34 +0200

    Add token-saving login component

--- EShop.Frontend/ClientApp/src/App.js
+++ EShop.Frontend/ClientApp/src/App.js
@@ -4,7 +4,8 @@ import { Route } from 'react-router';
 import { Home } from './components/Home/';
 import { ProductDetails } from './components/ProductDetails';
 import { Cart } from './components/Cart';
-import { Register } from './components/Register'
+import { Register } from './components/Register';
+import { Login } from './components/Login';
 
 export default class App extends Component {
   displayName = App.name
@@ -16,6 +17,7 @@ export default class App extends Component {
           <Route path='/product/:id' component={ProductDetails}/>
           <Route path='/cart' component={Cart}/>
           <Route path='/register' component={Register}/>
+          <Route path='/login' component={Login}/>
         </div>
     );
   }
---
+++ EShop.Frontend/ClientApp/src/components/Login/index.jsx
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import * as env from '../../env.js';
+
+//i am actually ashamed of myself this is exactly the same code as in <Register/>
+export class Login extends Component {
+    constructor(props) {
+        super(props);        
+        this.state = {
+            fields: {
+                email: '',
+                password: '',
+            },
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        e.persist();
+        this.setState(prevState => ({
+                fields: {
+                    ...prevState.fields,
+                    [e.target.name]: e.target.value 
+                }
+            }
+        ));
+    }
+    
+    //TODO add input validation
+    async handleSubmit(e) {
+        e.preventDefault();
+        const fields = this.state.fields;
+        
+        const requestBody = {
+            [env.userRegister.password]: fields.password,
+            [env.userRegister.email]: fields.email,
+        };
+        
+        const fetchParams = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestBody)
+        }
+        
+        let token = await (await fetch(env.host+env.apiLogin, fetchParams)).json();
+        localStorage.setItem("token", token);
+    }
+    
+    //TODO add e-mail address verification
+    //TODO add password strength indicator (get one from npm probably)
+    //TODO add classNames for CSS
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <p>E-mail:<input type="text" onChange={this.handleChange} name="email"/></p>
+                <p>Hasło:<input type="password" onChange={this.handleChange} name="password"/></p>
+                <input type="submit"/>
+            </form>
+        )
+    }
+}
