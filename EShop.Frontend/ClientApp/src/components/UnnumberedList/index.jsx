e8cd3717172c80abdb59e96276f24d9f836bbdea
Artur T <atagisov@gmail.com>
2018-05-01 16:22:17 +0200

    Rewrite <Categories/> to dig thru api object recursively

--- EShop.Frontend/ClientApp/src/components/UnnumberedList/index.jsx
+++ EShop.Frontend/ClientApp/src/components/UnnumberedList/index.jsx
@@ -10,23 +10,40 @@ export default class UnnumberedList extends Component {
         super();
         this.state = {
             data: [],
+            isOpen: false,
         }
     }
     
-    componentDidMount() {
-        fetch(env.host + env.apiCategories)
-            .then(response=>response.json())
-            .then(json => this.setState( { data: json }));
+    async componentDidMount() {
+        if(!this.props.dataset && !this.props.categoryName) {
+            let categoriesJSON = await (await fetch(env.host + env.apiCategories)).json();
+            this.setState({data: categoriesJSON});
+        }
     }
 
     render() {
+        let bar = (this.props.dataset) ? this.props.dataset : this.state.data;
+        let foo = bar.map( parent =>{
+                if(parent.children.length == 0) {
+                    console.log(parent[env.category.name]);
+                    return (<li key={parent[env.category.id]}>{parent[env.category.name]}</li>);
+                }
+
+                return parent.children.map(child => {
+                    let content = (child.children) ? <UnnumberedList dataset={child.children} categoryName={child[env.category.name]}/> : child[env.category.name]; 
+                    return (
+                        <li key={child[env.category.id]}>
+                            {content}
+                        </li>
+                    )
+                })
+        });
+        //when you call the topmost <UnnumberedList/> component it doesn't have props passed to it - if it doesn't have props, it fetches all categories then recursively creates itself with props
+        let catName = (this.props.categoryName) ? this.props.categoryName : this.state.data[env.category.name];
         
-        const categoriesList = this.state.data.map(category=>(
-            <li key={category[env.category.id]} className={style.listItem}>
-                {category[env.category.name]}
-            </li>
-        ));
-        
-        return <ul className={style.list}>{categoriesList}</ul>;
+        return (<ul className={style.list}>
+            {catName}    
+            {foo}
+            </ul>)
     }
 };
