d44f8d2713ea1c6e0140f0075bf570732a673483
Artur T <atagisov@gmail.com>
2018-05-07 13:47:58 +0200

    Fix <ProductDetails/> not refetching on id prop change
    +some refactoring/readability fixes
    +Minor CSS animation fix

--- EShop.Frontend/ClientApp/src/components/ProductDetails/index.jsx
+++ EShop.Frontend/ClientApp/src/components/ProductDetails/index.jsx
@@ -12,16 +12,22 @@ export class ProductDetails extends Component {
     constructor(props) {
         super(props);
         
-        let initialQuantity = this.props["initialQuantity"] ? this.props["initialQuantity"] : 1;
+        this.idRouteParamFunc = props => props["id"] ? props.id : props.match.params.id; //this has the "props" arg because its later used in componentWilLReceiveProps to get the same value as here, but from newProps
+        this.initialQuantity = props => props["initialQuantity"] ? this.props["initialQuantity"] : 1; //if value is in props, use props, if not use "1".
+        this.initialAdded = () => this.props["initialAdded"] ? this.props["initialAdded"] : false;
 
+        this.runPriceCallback = val => { if(this.props["totalPriceCallback"]) this.props.totalPriceCallback(val) };
+
+        
         this.state = {
             product: {},
-            numberOfCopiesToBuy: initialQuantity, //if value is in props, use props, if not use "1".
-            lastNumberOfCopiesToBuy: initialQuantity,
-            alreadyAddedToCart: this.props["initialAdded"] ? this.props["initialAdded"] : false,
+            numberOfCopiesToBuy: this.initialQuantity(this.props), 
+            lastNumberOfCopiesToBuy: this.initialQuantity(this.props),
+            alreadyAddedToCart: this.initialAdded(this.props),
         }
 
-        this.idRouteParam = this.props.id; // this is the ID of the product this component views (from URL - localhost/product/[:id], or if it doesnt exist, take from props)        
+        this.idRouteParam = this.idRouteParamFunc(this.props);
+        
         this.currency = env.currency;
 
         this.addProductToCart = this.addProductToCart.bind(this);
@@ -32,17 +38,17 @@ export class ProductDetails extends Component {
     async componentDidMount() {
         let _product = await(await fetch(env.host+env.apiSingleProduct+this.idRouteParam)).json();
         this.setState({product: _product});
-        if(this.props["totalPriceCallback"]) this.props.totalPriceCallback(this.state.product.price*this.state.lastNumberOfCopiesToBuy);
+        this.runPriceCallback(this.state.product.price*this.state.lastNumberOfCopiesToBuy);
     }
 
     async addProductToCart() {
-        this.setState({alreadyAddedToCart: true});
+        this.setState({ alreadyAddedToCart: true });
         
         let guid = this.getGuid(env.guidCookieName);
 
         if (!guid) {       
             function uuidv4() {
-                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
                   var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
                 return v.toString(16);
                 });
@@ -64,16 +70,15 @@ export class ProductDetails extends Component {
                 'Content-Type': 'application/json',
             },
         }
-    let responseCode = await(await fetch(env.host+env.apiCartRedis, requestBody)).status; //send items to redis
-    if(responseCode===200)
-        if(this.props["totalPriceCallback"]) 
-            this.props.totalPriceCallback(this.state.product.price*this.state.numberOfCopiesToBuy);
-    return responseCode===200; //return true if added successfully
+        let responseCode = await (await fetch(env.host + env.apiCartRedis, requestBody)).status; //send items to redis
+        if (responseCode === 200)
+            this.runPriceCallback(this.state.product.price * this.state.numberOfCopiesToBuy);
+        return responseCode === 200; //return true if added successfully
     }
     
     async removeProductFromCart() {
         
-        if(this.state.alreadyAddedToCart === false) { //if trying to remove product from cart even though the product is not in the cart
+        if (this.state.alreadyAddedToCart === false) { //if trying to remove product from cart even though the product is not in the cart
             throw new Error("Tried to remove product from cart, but state.alreadyAddedToCart says the product is not in the cart");
         } 
         let guid = this.getGuid(env.guidCookieName);
@@ -87,36 +92,53 @@ export class ProductDetails extends Component {
 
         console.log(requestParams);
         
-        let responseCode = await (await fetch([env.host, env.apiCartRedisDelete.slice(0,-1),'?', env.redisCartElement.key, '=', guid, '&', env.redisCartElement.id, '=', this.idRouteParam].join(''), requestParams)).status;
-        if(responseCode===200) this.props.totalPriceCallback(0); //if you remove the product from cart, the total price is 0
+        let responseCode = await (await fetch([env.host, env.apiCartRedisDelete.slice(0, -1), '?', env.redisCartElement.key, '=', guid, '&', env.redisCartElement.id, '=', this.idRouteParam].join(''), requestParams)).status;
+        if(responseCode===200)
+            this.runPriceCallback(0);
 
-        return responseCode===200; //return true if removed successfully
+        return responseCode === 200; //return true if removed successfully
+    }
+
+    //this needed to be implemented because when you <Redirect/>ed from route eg. `/product/25' to 'product/27' this component still rendered product id
+    //see https://stackoverflow.com/questions/32261441/component-does-not-remount-when-route-parameters-change
+    async componentWillReceiveProps(newProps) {
+        this.idRouteParam = this.idRouteParamFunc(newProps); //get new id from route or props
+        let _product = await(await fetch(env.host+env.apiSingleProduct+this.idRouteParam)).json();
+
+        this.setState({
+            product: _product,
+            numberOfCopiesToBuy: this.initialQuantity(newProps), 
+            lastNumberOfCopiesToBuy: this.initialQuantity(newProps),
+            alreadyAddedToCart: this.initialAdded(newProps),
+        });
     }
 
     render() {
         
     let fetchedProduct = this.state.product;
         
+    const {img, name, price, description} = env.product;    
+    
         return (
         <div className={style.productWrapper}>
             <div className={style.product}>
                 
                 <div className={style.imageWrapper}>
-                    <img src={fetchedProduct[env.product.img]} alt={fetchedProduct[env.product.name]} />
+                    <img src={fetchedProduct[img]} alt={fetchedProduct[name]} />
                 </div>
                 
                 <div className={style.infoWrapper}>
                     <div className={style.name}>
-                        <b>{fetchedProduct[env.product.name]}</b>
+                        <b>{fetchedProduct[name]}</b>
                     </div>
 
                     <div className={style.price}>
-                        {env.formatPrice(String(fetchedProduct[env.product.price])) + ' ' + this.currency}
+                        {env.formatPrice(String(fetchedProduct[price])) + ' ' + this.currency}
                     </div>
                 
                     <div className={style.description}>
                         <div><b>Opis:</b></div>
-                        {fetchedProduct[env.product.description]}
+                        {fetchedProduct[description]}
                     </div>
             
                     <div className={style.counterWrapper}>
