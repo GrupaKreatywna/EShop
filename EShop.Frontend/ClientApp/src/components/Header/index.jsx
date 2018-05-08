65cf1244ee6ca73b059c609ca83443f491f26793
Artur T <atagisov@gmail.com>
2018-05-08 22:57:36 +0200

    Fix <Header/> CSS design, move login logic to <LoginStatus/>

--- EShop.Frontend/ClientApp/src/components/Header/index.jsx
+++ EShop.Frontend/ClientApp/src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import { NavLink,Link } from 'react-router-dom';
 import SearchAutocomplete from '../SearchAutocomplete';
 
 import * as env from '../../env.js';
@@ -10,8 +10,7 @@ export class Header extends Component {
         super();
         
         this.state = {
-            cartExists: localStorage.getItem(env.guidCookieName) ? true : false, //TODO use redux to track cart state (number of items etc.)
-            userLoggedIn: localStorage.getItem(env.tokenCookieName) ? true : false,
+            
         }
     }
         //TODO make <Links> have className indicating that we are on the current route
@@ -19,33 +18,59 @@ export class Header extends Component {
         
         return(
             <div className={style.header}>
-                <div className={style.button + ' ' + style.logo}>
-                    <Link to='/'><img src="https://unsplash.it/100?random" alt="EShop"/></Link>
+                <div className={style.logo}>
+                    <Link to='/'>EShop</Link>
                 </div>
-                <Link to='/' className={style.button}>Home</Link>
-                <Link to='/cart' className={style.button}>Koszyk</Link>
-                <div className={ style.button + ' ' + style.searchAutocompleteWrapper} >
+                {/*<NavLink 
+                    to='/' 
+                    activeClassName={style["button--activeroute"]} 
+                    className={style.button}>
+                        Home
+                </NavLink> */}
+                
+                <NavLink 
+                    to='/cart' 
+                    activeClassName={style["button--activeroute"]} 
+                    className={style.button}>
+                    Koszyk
+                </NavLink>
+                
+                <div className={ style.searchAutocompleteWrapper} >
                     <SearchAutocomplete/>
                 </div>
-                <LoginStatus isLoggedIn={this.state.userLoggedIn}/>
+                <LoginStatus/>
             </div>
             )
         }
 
 }
 
-const LoginStatus = props => {
-    let returnValue;
+class LoginStatus extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            user: undefined,
+            cartExists: localStorage.getItem(env.guidCookieName), //TODO use redux to track cart state (number of items etc.)
+            userLoggedIn: localStorage.getItem(env.tokenCookieName),
+        }
+    }
     
-    if(props.isLoggedIn)
-        returnValue = (<div className={style.loginContainer + ' ' + style.button}>Zalogowano jako __PLACEHOLDER__</div>);
-    
-    else
-        returnValue = (
-            <div className={style.loginContainer}>
-                <Link to='/login' className={style.button + ' ' + style.loginInfo}>Logowanie</Link>
-                <Link to ='/register' className={style.button + ' ' + style.loginInfo}>Rejestracja</Link>
-            </div>)
+        componentDidMount() {
+            const requestParams = {
+                method:'GET'
+            }
+        }
 
-    return returnValue;
+    render() {
+        let returnValue = '';
+        if(this.state.userLoggedIn)
+            returnValue = (<div className={style.loginContainer}>Zalogowano jako __PLACEHOLDER__</div>);
+        else returnValue = (
+            <div className={style.loginContainer}>
+                <Link to='/login' className={style.loginInfo}>Logowanie</Link>
+                <Link to ='/register' className={style.loginInfo}>Rejestracja</Link>
+            </div>)
+        return returnValue;
+    }
 }
