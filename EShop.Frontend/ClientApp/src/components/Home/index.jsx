0369ad8d7a38d98a51f93d9b49ad39d766775c0f
Artur T <atagisov@gmail.com>
2018-05-08 22:58:35 +0200

    Remove bug reminder, move <Products/> higher up in the layout

--- EShop.Frontend/ClientApp/src/components/Home/index.jsx
+++ EShop.Frontend/ClientApp/src/components/Home/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import style from './style.css';
 import * as env from '../../env.js';
 
-import UnnumberedList from '../UnnumberedList/'; //TODO: webpack aliases (to reove the "../")
+import {CategoryWrapper} from '../UnnumberedList/'; //TODO: webpack aliases (to reove the "../")
 import SearchAutocomplete from '../SearchAutocomplete';
 import {Products} from '../Products/';
 
@@ -11,17 +11,11 @@ export const Home = () => (
   <div className={style.Layout}>
 
     <div className={style.gridCategories}>
-      <b>I am broken and I do not render parentId=NULL categories</b>
-      <UnnumberedList/>
+      <CategoryWrapper/>
     </div>
-    
-    <div className={style.gridSearchAutocomplete}>  
-
-    </div>
-
     <div className={style.gridProductsMostPopular}>  
       <p><b>Najczęściej kupowane</b></p>
-      <Products apiLink={env.apiMostBoughtProducts}/>
+      <Products apiLink={env.apiRecentlyAddedProducts}/>
     </div>
     
     <div className={style.gridProductsRecentlyAdded}>
