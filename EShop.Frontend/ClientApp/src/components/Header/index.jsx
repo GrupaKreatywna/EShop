9ede8011f2e063973cd0e6ff2f1c8179f74d9b3f
Artur T <atagisov@gmail.com>
2018-05-10 16:59:30 +0200

    Add Logout button

--- EShop.Frontend/ClientApp/src/components/Header/index.jsx
+++ EShop.Frontend/ClientApp/src/components/Header/index.jsx
@@ -50,22 +50,49 @@ class LoginStatus extends Component {
         super(props);
         
         this.state = {
-            user: undefined,
+            user: {},
             cartExists: localStorage.getItem(env.guidCookieName), //TODO use redux to track cart state (number of items etc.)
             userLoggedIn: localStorage.getItem(env.tokenCookieName),
         }
+
+        this.onLogoutClick = this.onLogoutClick.bind(this);
     }
     
-        componentDidMount() {
-            const requestParams = {
-                method:'GET'
+    async componentDidMount() {
+        if (!this.state.userLoggedIn) return;
+
+        this.setState({ userLoggedIn: localStorage.getItem(env.tokenCookieName) });
+        const requestParams = {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + this.state.userLoggedIn,
+                'Content-Type': 'application/x-www-form-urlencoded',
             }
+        };
+
+        let userInfo = await fetch(env.host + env.apiUserInfo, requestParams);
+        let responseCode = await userInfo.status;
+
+        if (responseCode === 200)
+            this.setState({ user: await userInfo.json() });
+        else if (responseCode === 401) { //session expired
+            localStorage.clear(env.tokenCookieName);
+            this.setState({ userLoggedIn: null });
         }
+    }
+
+    onLogoutClick() {
+        localStorage.clear(env.tokenCookieName);
+        this.setState({
+            userLoggedIn: undefined,
+        });
+        window.location.reload();
+    }
 
     render() {
         let returnValue = '';
         if(this.state.userLoggedIn)
-            returnValue = (<div className={style.loginContainer}>Zalogowano jako __PLACEHOLDER__</div>);
+            returnValue = (<div className={style.loginContainer}>Zalogowano jako {this.state.user["email"] || "?"} <a onClick={this.onLogoutClick} className={style.clickable}>Wyloguj</a></div>);
         else returnValue = (
             <div className={style.loginContainer}>
                 <Link to='/login' className={style.loginInfo}>Logowanie</Link>
